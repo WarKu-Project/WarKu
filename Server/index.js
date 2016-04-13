e21fc62419982e43abd8cc84fd0685a3be23fbd7
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-04-13 18:08:43 +0700

    Function to login and register of socket

--- Server/index.js
+++ Server/index.js
@@ -32,10 +32,44 @@ var server = app.listen(port,function() {
 /** initialize socket.io **/
 var io = require('socket.io').listen(server);
 
+/** Intitlize Project Module
+* player.js
+*/
+var player = require('player');
+
+/**
+* Server variable
+* online_user
+*/
+/** online_user **/
+var online_user = {};
+
 /** connect socket with io **/
 io.on('connection',function(socket){
   //get client ip
   var address = socket.handshake.address;
+  //notice client has connected
   console.log('Client IP = '+address.address+":"+address.port+' is connected.');
-   
+
+  /**
+  * Received function
+  **/
+  /** Fuction to regist new player **/
+  socket.on('register',function(info) {
+    if (!player.authorized(info)) {
+      player.createPlayer(info);
+      socket.emit('register-status',true);
+    } else {
+      socket.emit('register-status',false);
+    }
+  });
+  /** Function to login **/
+  socket.on('login',function(info) {
+    if (player.authorized(info)){
+      online_user[socket.address.address+":"+socket.address.port] = info["username"];
+      socket.emit('login-status',true);
+    }else {
+      socket.emit('login-status',false);
+    }
+  });
 });
