8b928b8b83d15f691d02618dc1629d0fb1bb4f74
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-04-19 00:17:57 +0700

    install js which is init some table(player villege resource structure building wall

---
+++ Server/install.js
@@ -0,0 +1,51 @@
+/**
+* Initialize mysql Module
+*/
+var mysql = require('mysql');
+
+/** Set up connection of mysql **/
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "warku"
+});
+
+/** Initialize player table **/
+con.query('CREATE TABLE player (pid int NOT NULL ,  username varchar(20) NOT NULL,  password varchar(20) NOT NULL,  email varchar(100) NOT NULL,  PRIMARY KEY(pid))',function(err) {
+  if (err) console.log(err.toString());
+  else console.log('player table is created to MySQL');
+});
+
+/** Initialize villege table **/
+con.query('CREATE TABLE villege (vid int NOT NULL,villege_name varchar(30) NOT NULL,wood int NOT NULL,clay int NOT NULL,iron int NOT NULL,crop int NOT NULL,pid int NOT NULL,PRIMARY KEY(vid),FOREIGN KEY(pid) REFERENCES player(pid))',function(err) {
+    if (err) console.log(err.toString());
+    else console.log('villege table is created to MySQL');
+});
+
+/** Initialize structure table (use as superclass of resource ,building and wall) **/
+con.query('CREATE TABLE structure (sid int NOT NULL,level int NOT NULL,vid int NOT NULL,PRIMARY KEY(sid),FOREIGN KEY(vid) REFERENCES villege(vid))',function(err) {
+  if(err) console.log(err.toString());
+  else console.log('structure table is created to MySQL');
+});
+
+/** Initialize resource table **/
+con.query('CREATE TABLE resource (sid int NOT NULL,level int NOT NULL,resource_pos int NOT NULL,resource_type varchar(20) NOT NULL,PRIMARY KEY(sid),FOREIGN KEY(sid,level) REFERENCES structure(sid,level))',function(err) {
+  if (err) console.log(err.toString());
+  else console.log('resource table is created to MySQL');
+});
+
+/** Initialize building table **/
+con.query('CREATE TABLE building (sid int NOT NULL,level int NOT NULL,building_pos int NOT NULL,building_type varchar(20) NOT NULL,PRIMARY KEY(sid),FOREIGN KEY(sid,level) REFERENCES structure(sid,level))',function(err) {
+  if (err) console.log(err.toString());
+  else console.log('building table is created to MySQL');
+})
+
+/** Initialize wall table **/
+con.query('CREATE TABLE wall (sid int NOT NULL,level int NOT NULL,wall_pos varchar(20) NOT NULL,PRIMARY KEY(sid),FOREIGN KEY(sid,level) REFERENCES structure(sid,level))',function(err) {
+  if (err) console.log(err.toString());
+  else console.log('wall table is created to MySQL');
+})
+
+/** Initialize troop table **/
+//con.query('CREATE TABLE unit ()')
